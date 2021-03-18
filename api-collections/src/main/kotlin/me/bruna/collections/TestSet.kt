c87c83c04cb4045f92592d437902b4baad8ed601
brunahelenas <shbhelena@gmail.com>
2021-03-18 19:32:21 -0300

    Adicionando novas arquivos e classes

--- api-collections/src/main/kotlin/me/bruna/collections/TestSet.kt
+++ api-collections/src/main/kotlin/me/bruna/collections/TestSet.kt
@@ -1,11 +1,24 @@
 package me.bruna.collections
 
-fun main(){
+fun main() {
 
     val joao = Funcionario(nome = "Joao", salario = 2000.0, tipoContratacao = "CLT")
     val pedro = Funcionario(nome = "Pedro", salario = 1500.0, tipoContratacao = "PJ")
     val maria = Funcionario(nome = "Maria", salario = 4000.0, tipoContratacao = "CLT")
 
-    val funcionarios1= setOf(joao,pedro)
+    val funcionarios1 = setOf(joao, pedro)
     val funcionarios2 = setOf(maria)
+
+    val resultUnion = funcionarios1.union(funcionarios2)
+    resultUnion.forEach { println(it) }
+
+    println("-----------------------------")
+
+    val funcionarios3 = setOf(joao, pedro, maria)
+    val resultSubtract = funcionarios3.subtract(funcionarios2)
+    resultSubtract.forEach { println(it) }
+
+    val resultIntersect = funcionarios3.intersect(funcionarios2)
+    resultIntersect.forEach { println(it) }
+
 }
